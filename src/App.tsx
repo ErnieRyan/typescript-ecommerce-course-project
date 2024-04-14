8cb5b54eb695c2844fa234429a3209c34e1d02cd
Ryan Esmores <ryanesmores07@gmail.com>
2024-04-15 00:21:29 +0900

    Implemented Tailwind Custom Class and created Header component

--- src/App.tsx
+++ src/App.tsx
@@ -56,16 +56,9 @@ const router = createBrowserRouter([
 
 const App = () => {
   return (
-    <div>
+    <RouterProvider router={router}>
       <h1 className="text-7xl font-bold ">App</h1>
-      <Button
-        variant={"destructive"}
-        size={"lg"}
-        onClick={() => console.log("it worked!!!")}
-      >
-        Click Me
-      </Button>
-    </div>
+    </RouterProvider>
   );
 };
 export default App;
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -5,7 +5,6 @@ const Cart = () => {
   return (
     <>
       <h1 className="text-4xl">Cart</h1>
-
       <Button asChild size={"lg"}>
         <Link to="/">back home</Link>
       </Button>
