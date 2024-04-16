e4f9ae3aa8efd8813d4792249a087952223b2a52
Ryan Esmores <ryanesmores07@gmail.com>
2024-04-17 00:29:06 +0900

    Implemented ErrorPage and CartButton Component

--- src/App.tsx
+++ src/App.tsx
@@ -52,10 +52,12 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+    errorElement: <Error />,
   },
   {
     path: "/register",
     element: <Register />,
+    errorElement: <Error />,
   },
 ]);
 
