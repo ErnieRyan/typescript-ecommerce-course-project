1cfdab58d3a95753207ba0761503e04a33a03e19
Ryan Esmores <ryanesmores07@gmail.com>
2024-04-24 00:11:42 +0900

    Completed Add to cart functionality. Continue with Cart Page

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -4,12 +4,15 @@ import {
   customFetch,
   formatAsDollars,
   type SingleProductResponse,
+  type CartItem,
 } from "@/utils";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { SelectProductAmount, SelectProductColor } from "@/components";
 import { Mode } from "@/components/SelectProductAmount";
+import { useAppDispatch } from "@/hooks";
+import { addItem } from "@/features/cart/cartSlice";
 
 export const loader: LoaderFunction = async ({
   params,
@@ -25,13 +28,24 @@ const SingleProduct = () => {
   const { data: product } = useLoaderData() as SingleProductResponse;
   const { image, title, price, description, colors, company } =
     product.attributes;
-
   const dollarsAmount = formatAsDollars(price);
   const [productColor, setProductColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
+  const dispatch = useAppDispatch();
+
+  const cartProduct: CartItem = {
+    cartID: product.id + productColor,
+    productID: product.id,
+    image,
+    title,
+    price,
+    amount,
+    productColor,
+    company,
+  };
 
   const addToCart = () => {
-    console.log("add to cart");
+    dispatch(addItem(cartProduct));
   };
 
   return (
